f3c661b66c12a7bbda27d24ede99dc6cd4261218
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-03-06 12:32:17 -0800

    Connected to mongoose, wrote out repoSchema

--- database/index.js
+++ database/index.js
@@ -1,10 +1,23 @@
+//is required, mongoose.connect connects to the server... after localhost/ is the name of the database you want
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
-
-let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+mongoose.connect('mongodb://localhost/fetcher', {useNewUrlParser: true});
+//startes the mongoose connection
+var db =  mongoose.connection;
+//this connection is pending
+//db.on is saying on an error... run whats next
+db.on('error') console.error.bind(console, 'connection error: '));
+//db.once is saying onces file is open ..run whats next
+db.once('open',function(){
+  console.log('MongoDB has been connected!')
+})
+//all is derived from Schema in mongoose
+let repoSchema = new mongoose.Schema({
+  username: String,
+  description: String,
+  language: String,
+  size: Number
 });
-
+//first argument is a singular name of the collection, second is the schema layout for the collection
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (/* TODO */) => {

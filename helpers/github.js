6bb72decdad2f25d03d03b75ab17f181227f0942
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-03-06 19:56:22 -0800

    HTTP request is receiving data from HitHUB API

--- helpers/github.js
+++ helpers/github.js
@@ -1,20 +1,43 @@
 const request = require('request');
 const config = require('../config.js');
+const axios = require('axios');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username) => {
   // TODO - Use the request module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
   let options = {
-    url: 'FILL ME IN',
+    method: 'GET',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
-    }
-  };
+    } }
 
-}
+    function callback( error, response, body) {
+      if(!error && response.statusCode == 200){
+        console.log('body: ', JSON.parse(body))
+
+      }
+    }
+    request(options, callback)
+  }
 
 module.exports.getReposByUsername = getReposByUsername;
+
+
+// then((response)=>{
+//       response.map((repo)=>(
+//         {
+//           username: repo.owner.login,
+//           description: repo.description,
+//           size: repo.size,
+//           forks: repo.fork,
+//           language: repo.language
+//         })
+//           .catch((err)=> console.error(err))
+//       })
+//       axios(options);
+//   };
